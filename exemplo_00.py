b484c9e053fc752faf8e4dd612f629ece2767c8c
Gilkleber Medeiros <gikimedei2018@gmail.com>
2025-08-03 16:14:19 -0300

    first commit!

---
+++ exemplo_00.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+st.markdown("""
+    # Olá Internet!!
+            
+    **Negrito**, *Itálico* 
+            
+    ## Olá, este título é equivalente à um h2 do html
+""")
+
+st.image(r"https://placehold.co/600x400?text=Olá+Internet!!\n+Imagem+streamlit!!")
